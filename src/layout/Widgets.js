1e4cb739eaf1f6dc7ff8b6660c9f0a3458acc72c
haguse <halitguvens@gmail.com>
2021-08-07 19:15:54 +0300

    Add Search Bar

--- src/layout/Widgets.js
+++ src/layout/Widgets.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { SearchIcon } from "../icons/icon";
 
 const Widgets = () => {
   return (
-    <aside className="bg-purple-200 w-80">
-      <h1>Timeline</h1>
+    <aside className="w-80">
+      <div className="flex items-center space-x-2 p-2 m-2 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
+        <SearchIcon className="w-4 h-4" />
+        <input
+          type="search"
+          placeholder="Search Twitter"
+          className="placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm focus-within:"
+        />
+      </div>
+      <div className="mt-5"></div>
     </aside>
   );
 };
