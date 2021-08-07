27e1bb30b9329c12bdf9f6ae4eb6d256da16aae1
haguse <halitguvens@gmail.com>
2021-08-07 13:39:20 +0300

    Color Configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          base: "#1DA1F2",
+          dark: "hsl(203, 89%, 46%)",
+          light: "hsl(203, 89%, 94%)",
+        },
+        gray: {
+          dark: "#657786",
+          light: "#AAB8C2",
+          extraLight: "#E1E8ED",
+          lightets: "#F5F8FA",
+        },
+        black: "#14171A",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
